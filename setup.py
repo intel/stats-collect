ea057e9db72116bdbdea130b093546f570e0a5b7
Adam Hawley <adam.james.hawley@intel.com>
2023-04-20 14:42:48 +0300

    setup.py: install web-assets in '/usr/share/javascript'
    The previous installation destination was not correct for fedora
    packaging (which is the reason it was originally changed). This patch
    fixes the web-assets installation directory.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     data_files=get_data_files("share/stats-collect/helpers/stc-agent", "helpers/stc-agent",
                               exclude=_PYTHON_HELPERS) + \
                get_data_files("share/stats-collect/defs/statscollect", "defs/statscollect") + \
-               get_data_files("share/web-assets/javascript/stats-collect/js/dist", "js/dist") + \
+               get_data_files("share/javascript/stats-collect/js/dist", "js/dist") + \
                get_data_files("share/stats-collect/misc/servedir", "misc/servedir") + \
                [("share/stats-collect/js", ["js/index.html"])],
     scripts=_TOOLNAMES + _PYTHON_HELPERS,
