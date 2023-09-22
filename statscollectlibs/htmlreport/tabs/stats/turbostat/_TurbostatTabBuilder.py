e6c14e4e1679f7b70e828339d2aa232478941870
Adam Hawley <adam.james.hawley@intel.com>
2023-09-22 21:11:19 +0300

    _TurbostatTabBuilder: add 'get_default_tab_cfg()' method
    This patch adds the 'get_default_tab_cfg()' method to
    'TurbostatTabBuilder' so that it is consistent with other tab builder
    classes. In addition, this patch also facilitates making the turbostat
    default tab configurations accessible through the 'StatsTabBuilder'
    class.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- statscollectlibs/htmlreport/tabs/stats/turbostat/_TurbostatTabBuilder.py
+++ statscollectlibs/htmlreport/tabs/stats/turbostat/_TurbostatTabBuilder.py
@@ -11,8 +11,8 @@ This module provides the capability of populating the turbostat statistics tab.
 """
 
 import logging
-from pepclibs.helperlibs.Exceptions import ErrorNotFound
-from statscollectlibs.htmlreport.tabs import _Tabs
+from pepclibs.helperlibs.Exceptions import ErrorNotFound, Error
+from statscollectlibs.htmlreport.tabs import _Tabs, TabConfig
 from statscollectlibs.htmlreport.tabs.stats.turbostat import _MCPUL2TabBuilder, _TotalsL2TabBuilder
 
 _LOG = logging.getLogger()
@@ -22,12 +22,23 @@ class TurbostatTabBuilder:
     This class provides the capability of populating the turbostat statistics tab.
 
     Public methods overview:
-    1. Generate a '_Tabs.CTabDC' instance containing turbostat level 2 tabs.
+    1. Optionally, retrieve the default 'TabConfig.CTabConfig' instance. See 'TabConfig' for more
+       information on tab configurations.
+       * 'get_default_tab_cfg()'
+    2. Generate a '_Tabs.CTabDC' instance containing turbostat level 2 tabs. Optionally provide a
+       tab configuration as a 'CTabConfig' to customise the tab. This can be based on the default
+       configuration retrieved using 'get_default_tab_cfg()'.
        * 'get_tab()'
     """
 
     name = "Turbostat"
     stname = "turbostat"
+
+    def get_default_tab_cfg(self):
+        """Same as 'TabBuilderBase.get_default_tab_cfg()'."""
+
+        ctabs = [tbldr.get_default_tab_cfg() for tbldr in self.l2tab_bldrs.values()]
+        return TabConfig.CTabConfig(self.name, ctabs=ctabs)
 
     def get_tab(self, tab_cfg=None):
         """
@@ -41,11 +52,22 @@ class TurbostatTabBuilder:
         """
 
         l2_tabs = []
-        for stab_bldr in self.l2tab_bldrs:
-            l2_tabs.append(stab_bldr.get_tab(tab_cfg=tab_cfg))
 
-        return _Tabs.CTabDC(self.name, l2_tabs)
+        if not tab_cfg:
+            for stab_bldr in self.l2tab_bldrs.values():
+                l2_tabs.append(stab_bldr.get_tab())
+            return _Tabs.CTabDC(self.name, l2_tabs)
 
+        # Turbostat tab configs must contain either 'Measured CPU' or 'Totals' container tabs.
+        for l2tab_cfg in tab_cfg.ctabs:
+            try:
+                l2_tabs.append(self.l2tab_bldrs[l2tab_cfg.name].get_tab(tab_cfg=l2tab_cfg))
+            except KeyError:
+                l2_names = ", ".join(bldr.name for bldr in self.l2tab_bldrs)
+                raise Error(f"{self.name} tab configuration should contain one or both of the "
+                            f"following container tabs: {l2_names}") from None
+
+        return _Tabs.CTabDC(tab_cfg.name, l2_tabs)
 
     def __init__(self, rsts, outdir, basedir=None):
         """
@@ -58,15 +80,15 @@ class TurbostatTabBuilder:
                   should be included in the turbostat tabs.
         """
 
-        self.l2tab_bldrs = []
+        self.l2tab_bldrs = {}
 
         try:
-            self.l2tab_bldrs.append(_MCPUL2TabBuilder.MCPUL2TabBuilder(rsts, outdir / self.name,
-                                                                       basedir=basedir))
+            mcpu_bldr = _MCPUL2TabBuilder.MCPUL2TabBuilder
+            self.l2tab_bldrs[mcpu_bldr.name] = mcpu_bldr(rsts, outdir / self.name, basedir=basedir)
         except ErrorNotFound:
-            _LOG.info("No measured CPUs specified for any results so excluding 'Measured CPU' "
-                      "turbostat tab.")
+            _LOG.info("No measured CPUs specified for any results so excluding '%s' %s tab.",
+                      mcpu_bldr.name, self.name)
 
 
-        self.l2tab_bldrs.append(_TotalsL2TabBuilder.TotalsL2TabBuilder(rsts, outdir / self.name,
-                                                                       basedir=basedir))
+        totals_bldr = _TotalsL2TabBuilder.TotalsL2TabBuilder
+        self.l2tab_bldrs[totals_bldr.name] = totals_bldr(rsts, outdir / self.name, basedir=basedir)
