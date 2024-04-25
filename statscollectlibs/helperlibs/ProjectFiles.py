e3a82fba9f813b340bb40ebf07399730a0c32c57
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2024-04-25 09:49:08 +0300

    ProjectFiles: adjust to 'pepc.ProjectFiles' changes
    The env_var argument became 'envvars' and should specify a collection of paths
    now.
    
    While on it, make sure the kwargs arguments are specified explicitly when
    calling 'search_project_data()'. Also rename the 'subpath' argument for
    consistency with recent 'pepc.ProjectFiles' changes.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- statscollectlibs/helperlibs/ProjectFiles.py
+++ statscollectlibs/helperlibs/ProjectFiles.py
@@ -21,19 +21,21 @@ def get_project_web_assets_envvar(prjname):
     name = prjname.replace("-", "_").upper()
     return f"{name}_WEB_ASSETS_PATH"
 
-def find_project_web_assets(prjname, subpath, pman=None, what=None):
+def find_project_web_assets(prjname, datadir, pman=None, what=None):
     """
     Search for project 'prjname' web assets. The arguments are as follows:
       * prjname - name of the project the web-asset belongs to.
-      * subpath - the sub-path of the web-asset in the web-asset project installation base
+      * datadir - the sub-path of the web-asset in the web-asset project installation base
                   directory.
+      * datadir - name of the sub-directory containing the web asset. This method basically
+                  searches for 'datadir' in a set of pre-defined paths (see below).
       * pman - the process manager object for the host to find the web-asset on (local host by
                default).
       * what - human-readable description of 'subpath' (or what is searched for), which will be used
                in the error message if an error occurs.
 
-    The web-assets are searched for in the 'subpath' sub-path of the following directories (and in
-    the following order).
+    The web-assets are searched for in the 'datadir' directory (or sub-path) of the following
+    directories (and in the following order).
       * in the directory the of the running program.
       * in the directory specified by the '<prjname>_WEB_ASSETS_PATH' environment variable.
       * in '$HOME/.local/share/javascript/<prjname>/', if it exists.
@@ -42,5 +44,5 @@ def find_project_web_assets(prjname, subpath, pman=None, what=None):
       * in '/usr/share/javascript/<prjname>/', if it exists.
     """
 
-    return next(search_project_data(f"javascript/{prjname}", subpath, pman, what,
-                                    get_project_web_assets_envvar(prjname)))
+    return next(search_project_data(f"javascript/{prjname}", datadir, pman=pman, what=what,
+                                    envvars=(get_project_web_assets_envvar(prjname),)))
