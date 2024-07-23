b8f695d986bc2dd31fa7d3c116b77af6561ea2a2
Adam Hawley <adam.james.hawley@intel.com>
2024-07-23 15:44:57 +0300

    DeployHelpersBase: remove hard-coded environment variable reference
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- statscollectlibs/deploylibs/DeployHelpersBase.py
+++ statscollectlibs/deploylibs/DeployHelpersBase.py
@@ -37,7 +37,8 @@ class DeployHelpersBase(DeployInstallableBase.DeployInstallableBase):
     def _get_helpers_deploy_path(self):
         """Returns path the directory the helpers should be deployed to."""
 
-        helpers_path = os.environ.get("STATS_COLLECT_HELPERSPATH")
+        envvar = ProjectFiles.get_project_helpers_envvar("stats-collect")
+        helpers_path = os.environ.get(envvar)
         if not helpers_path:
             helpers_path = self._spman.get_homedir() / HELPERS_DEPLOY_SUBDIR / "bin"
         return Path(helpers_path)
