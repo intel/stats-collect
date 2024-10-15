1d51ad0eb9600e721e0a2fdb0ef95bd4381b6df4
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2024-10-15 17:42:18 +0300

    defs: move '_mangle_placeholders' to 'TurbostatDefs' module
    The '_mangle_placeholders()' method is turbostat-specific. Move it to
    'TurbostatDefs'.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- statscollectlibs/defs/TurbostatDefs.py
+++ statscollectlibs/defs/TurbostatDefs.py
@@ -8,6 +8,7 @@
 
 """This module provides the API to turbostat metrics definitions (AKA 'defs')."""
 
+import re
 from statscollectlibs.defs import _STCDefsBase
 from statscollectlibs.parsers import TurbostatParser
 
@@ -111,6 +112,120 @@ class UncoreFreqDef:
 
 class TurbostatDefs(_STCDefsBase.STCDefsBase):
     """This module provides API to turbostat metrics definitions (AKA 'defs')."""
+
+    def _mangle_placeholders(self, placeholders_info):
+        """
+        Mangle the definitions by substituting placeholders with real values. The arguments are as
+        follows.
+          * placeholders_info - a list or tuple of dictionaries describing how to mangle the
+                                definitions by substituting placeholders with real values.
+
+        The 'placeholders_info' list has the following format.
+          [
+           { "placeholder"   : placeholder,
+             "values"        : list_of_values,
+             "casesensitive" : boolean_value},
+           ... etc ...
+          ]
+
+        Every dictionary in the list provides the following components to the mangler:
+          placeholder - the placeholder string that has to be substituted with elements from the
+                        'values' list.
+          values - list of values to substitute the placeholder with.
+          casesensitive - whether to use case-sensitive or case-insensitive matching and
+                          substitution. 'True' by default.
+
+        Example.
+
+        The definitions YAML file includes the 'CCx%' metric:
+
+           CCx%
+              title: "CCx residence"
+              description: "Time in percent spent in Cx core C-state. Matches turbostat CPU%cx."
+              unit: "%"
+
+        This metric describes core C-states. Core C-states are platform-dependent, so the real names
+        are not known in advance. Therefore, the definitions YAML file uses the 'CCx' placeholder
+        instead.
+
+        Suppose the platform has CC1 and CC6 C-states. The 'placeholders_info' list could be the
+        following in this case:
+          [ { "placeholder" : "Cx",
+              "values" : [ "C1", "C6" ],
+              "casesensitive" : False, } ]
+
+        The mangler would replace the 'CCx%' placeholder metric definition with the following.
+
+           CC1%
+              title: "CC1 residence"
+              description: "Time in percent spent in C1 core C-state. Matches turbostat CPU%c1."
+              unit: "%"
+           CC6%
+              title: "CC6 residence"
+              description: "Time in percent spent in C6 core C-state. Matches turbostat CPU%c6."
+              unit: "%"
+        """
+
+        # The sub-keys to look and substitute the placeholders in.
+        mangle_subkeys = { "title", "descr", "fsname", "name" }
+
+        for pinfo in placeholders_info:
+            values = pinfo["values"]
+            phld = pinfo["placeholder"]
+
+            # Whether matching and replacement should be case-sensitive or not.
+            # 1. Case-sensitive.
+            #   * Match metrics that include the placeholder in the name.
+            #   * Replace all placeholders with values in 'values'.
+            # 2. Case-insensitive.
+            #   * Match metrics that include the placeholder in the name, but accept both lower
+            #     and upper cases.
+            #   * Replace all placeholders with values in 'values'. But when replacing, if the
+            #     original value was in upper case, use upper case, otherwise use lower case
+            #     (preserve the case when replacing).
+            case_sensitive = pinfo.get("casesensitive", True)
+
+            if case_sensitive:
+                regex = re.compile(phld)
+            else:
+                regex = re.compile(phld, re.IGNORECASE)
+
+            for placeholder_metric in list(self.info):
+                if not regex.search(placeholder_metric):
+                    continue
+
+                # We found the placeholder metric (e.g., 'CCx%'). Build the 'replacement' dictionary
+                # which will replace the 'CCx' sub-dictionary with metric names (e.g., 'CC1' and
+                # 'CC6').
+                replacement = {}
+                for val in values:
+                    # pylint: disable=cell-var-from-loop,unnecessary-lambda-assignment
+                    if case_sensitive:
+                        func = lambda mo: val
+                    else:
+                        # The replacement function. Will replace with upper-cased or lower-cased
+                        # 'val' depending on whether the replaced sub-string starts with a capital
+                        # letter.
+                        func = lambda mo: val.upper() if mo.group(0)[0].isupper() else val.lower()
+
+                    metric = regex.sub(func, placeholder_metric)
+                    replacement[metric] = self.info[placeholder_metric].copy()
+
+                    for subkey, text in replacement[metric].items():
+                        if subkey in mangle_subkeys:
+                            replacement[metric][subkey] = regex.sub(func, text)
+
+                # Construct new 'self.info' by replacing the placeholder metric with the replacement
+                # metrics.
+                new_info = {}
+                for metric, minfo in self.info.items():
+                    if metric != placeholder_metric:
+                        new_info[metric] = minfo
+                    else:
+                        for new_metric, new_minfo in replacement.items():
+                            new_info[new_metric] = new_minfo
+
+                self.info = new_info
 
     def mangle_descriptions(self):
         """Mangle turbostat metric descriptions to describe how they are summarised by turbostat."""
