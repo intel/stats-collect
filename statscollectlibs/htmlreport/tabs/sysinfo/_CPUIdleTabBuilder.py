cc12c4005a37b149928d6901b5db4aeb19229a16
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-01-14 15:34:43 +0200

    _CPUIdleTabBuilder: refactor and annotate
    Add type hints, refactor to use new style.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- statscollectlibs/htmlreport/tabs/sysinfo/_CPUIdleTabBuilder.py
+++ statscollectlibs/htmlreport/tabs/sysinfo/_CPUIdleTabBuilder.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 tw=100 et ai si
 #
-# Copyright (C) 2022 Intel Corporation
+# Copyright (C) 2022-2025 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # Authors: Adam Hawley <adam.james.hawley@intel.com>
+#          Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
 
 """
-This module provides the capability of populating a "cpuidle" SysInfo tab to visualise information
-collected from '/sys/devices/system/cpu/cpuidle'.
+Provide API for populating the "cpuidle" sub-tab of the "SysInfo" container tab.
 """
 
+from pathlib import Path
 from statscollectlibs.htmlreport.tabs.sysinfo import _SysInfoTabBuilderBase
 
 _FILES = {
@@ -19,14 +20,17 @@ _FILES = {
 
 class CPUIdleTabBuilder(_SysInfoTabBuilderBase.SysInfoTabBuilderBase):
     """
-    This class provides the capability of populating a "cpuidle" info tab to visualise information
-    collected from '/sys/devices/system/cpu/cpuidle'.
-
-    Public method overview:
-     * get_tab() - returns a '_Tabs.DTabDC' instance which contains the collected information.
+    Provide API for populating the "cpuidle" sub-tab of the "SysInfo" container tab.
     """
 
-    def __init__(self, outdir, stats_paths, basedir=None):
-        """Class constructor. Arguments are the same as in 'DTabBuilderBase.__init__()'."""
+    def __init__(self, outdir: Path, stats_paths: dict[str, Path], basedir: Path | None = None):
+        """
+        The class constructor.
+
+        Args:
+            outdir: The output directory path (where the sub-tab files should be placed).
+            stats_paths: A dictionary mapping report IDs to raw statistics directory paths.
+            basedir: The report base directory directory path, defaults to 'outdir'.
+        """
 
         super().__init__("cpuidle", outdir, _FILES, stats_paths, basedir=basedir)
