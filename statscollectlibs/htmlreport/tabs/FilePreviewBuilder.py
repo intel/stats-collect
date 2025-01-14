c831f9fb667f67f3c1cbcef51b8b500cc2163a4b
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-01-14 18:52:17 +0200

    FIlePreviewBuilder: refactor and cleanup
    Use new coding style and conventions.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- statscollectlibs/htmlreport/tabs/FilePreviewBuilder.py
+++ statscollectlibs/htmlreport/tabs/FilePreviewBuilder.py
@@ -5,15 +5,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # Authors: Adam Hawley <adam.james.hawley@intel.com>
+#          Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
 
 """
-This module provides the capability of populating a 'File Preview'. See '_Tabs.FilePreviewDC' for
-more information on file previews.
+API for creating and populating file previews. A file preview is a data tab element that includes
+the contents of one or mutliple files, and possibly a diff between the files.
 """
 
 import difflib
 import filecmp
 import logging
+from pathlib import Path
 from pepclibs.helperlibs import Human
 from pepclibs.helperlibs.Exceptions import Error, ErrorExists
 from statscollectlibs.helperlibs import FSHelpers
@@ -21,39 +23,57 @@ from statscollectlibs.htmlreport.tabs import _Tabs
 
 _LOG = logging.getLogger()
 
-def _reasonable_file_size(fp, name):
+_RESONABLE_FILE_SIZE = 2 * 1024 * 1024
+
+def _has_reasonable_size(file_path: Path, title: str) -> bool:
     """
-    Returns 'True' if the file at path 'fp' is 2MiB or smaller, otherwise returns 'False'. Also
-    returns 'False' if the size could not be verified.  Arguments are as follows:
-        * fp - path of the file to check the size of.
-        * name - name of the file-preview being generated.
+    Check if a file that is intended to be included to a file preview has "reasonable" size.
+
+    Args:
+        file_path: path to the file to check the size for.
+        title: the file preview title.
+
+    Returns:
+        'True' if file size is reasonable, otherwise 'False'
     """
 
     try:
-        fsize = fp.stat().st_size
+        fsize = file_path.stat().st_size
     except OSError as err:
-        _LOG.warning("skipping file preview '%s': unable to check the size of file '%s' before "
-                     "copying:\n%s", name, fp, err)
+        errmsg = Error(err).indent(2)
+        _LOG.warning("Skipping file preview '%s': Unable to check the size of file '%s' before "
+                     "copying:\n%s", title, file_path, errmsg)
         return False
 
-    if fsize > 2*1024*1024:
-        _LOG.warning("skipping file preview '%s': the file '%s' (%s) is larger than 2MiB.",
-                     name, fp, Human.bytesize(fsize))
+    if fsize > _RESONABLE_FILE_SIZE:
+        _LOG.warning("Skipping file preview '%s': file '%s' size is %s, which is larger than the "
+                     "%s limit", title, file_path, Human.bytesize(fsize),
+                     Human.bytesize(_RESONABLE_FILE_SIZE))
         return False
+
     return True
 
 class FilePreviewBuilder:
-    """This class provides the capability of populating a 'File Preview'."""
+    """
+    Create and Populate file previews. A file preview is a data tab element that includes the
+    contents of one or mutliple files, and possibly a diff between the files.
+    """
 
-    def _generate_diff(self, paths, diff_name):
+    def _generate_html_diff(self, paths: list[Path], diff_name: str) -> Path:
         """
-        Helper function for '_add_fpreviews()'. Generates an HTML diff of the files at 'paths' with
-        filename 'diff_name.html' in a "diffs" sub-directory. Returns the path of the HTML diff
-        relative to 'outdir'.
+        Generate an HTML diff of the files at 'paths' with file name 'diff_name.html'.
+
+        Args:
+            paths: paths to the files to diff.
+            diff_name: base name of the resulting diff HTML file.
+
+        Returns:
+            Path of the resulting diff HTML file relative to the result base directory
+            ('self.basedir').
         """
 
-        if filecmp.cmp(*paths, shallow=False):
-            identical_diff_path = self.outdir / "diffs" / "identical.diff"
+        if filecmp.cmp(paths[0], paths[1], shallow=False):
+            identical_diff_path = self._outdir / "diffs" / "identical.diff"
             if not identical_diff_path.exists():
                 identical_diff_path.parent.mkdir(parents=True, exist_ok=True)
                 with open(identical_diff_path, "w", encoding="utf-8") as f:
@@ -68,15 +88,15 @@ class FilePreviewBuilder:
                     lines.append(f.readlines())
             except OSError as err:
                 msg = Error(err).indent(2)
-                raise Error(f"cannot open file at '{fp}' to create diff:\n{msg}") from None
+                raise Error(f"Cannot open file at '{fp}' to create diff:\n{msg}") from None
 
         # Store the diff in a separate directory and with the '.diff' file ending.
-        diff_path = (self.outdir / "diffs" / diff_name).with_suffix('.diff')
+        diff_path = (self._outdir / "diffs" / diff_name).with_suffix('.diff')
         try:
             diff_path.parent.mkdir(parents=True, exist_ok=True)
         except OSError as err:
             msg = Error(err).indent(2)
-            raise Error(f"cannot create diffs directory '{diff_path.parent}':\n"
+            raise Error(f"Cannot create diffs directory '{diff_path.parent}':\n"
                         f"{msg}") from None
 
         try:
@@ -86,85 +106,100 @@ class FilePreviewBuilder:
                                                   fromfile=reportids[0], tofile=reportids[1]))
         except Exception as err:
             msg = Error(err).indent(2)
-            raise Error(f"cannot create diff at path '{diff_path}':\n{msg}") from None
+            raise Error(f"Cannot create diff at path '{diff_path}':\n{msg}") from None
 
         return diff_path.relative_to(self._basedir)
 
-    def _copy_file(self, fp, reportid):
+    def _copy_file(self, file_path: Path, reportid: str) -> Path:
         """
-        Helper function for 'build_fpreview()'. Copies the file at path 'fp' to
-        'self.outdir / reportid'.
+        Copy file a file preview path from the raw result directory to the output directory.
+
+        Args:
+            file_path: the raw file preview file path.
+            reportid: report ID of the result the file belongs to.
+
+        Returns:
+            Path of the copy.
         """
 
-        dst_dir = self.outdir / reportid
+        dst_dir = self._outdir / reportid
 
         try:
             dst_dir.mkdir(parents=True, exist_ok=True)
         except OSError as err:
-            msg = Error(err).indent(2)
-            raise Error(f"can't create directory '{dst_dir}':\n{msg}") from None
+            errmsg = Error(err).indent(2)
+            raise Error(f"Can't create directory '{dst_dir}':\n{errmsg}") from err
 
-        dst_path = dst_dir / fp.name
+        dst_path = dst_dir / file_path.name
 
         try:
-            FSHelpers.copy(fp, dst_path)
+            FSHelpers.copy(file_path, dst_path)
         except ErrorExists:
-            _LOG.debug("file '%s' already in output dir: will not replace.", dst_path)
-            dst_path = fp
+            _LOG.debug("File '%s' is already in output dir: will not replace", dst_path)
+            dst_path = file_path
 
         return dst_path
 
-    def build_fpreview(self, title, files):
+    def build_fpreview(self, title: str, paths: dict[str, Path]) -> _Tabs.FilePreviewDC:
         """
-        Build file preview. Arguments are as follows:
-         * title - the title of the resultant file preview element.
-         * files - dictionary in the format '{ReportID: FilePath}' where 'FilePath' is the
-                   path to the file which should be included in the file preview for result
-                   'ReportID'.
+        Build and return a file preview element.
+
+        Args:
+            title: The title of the resultant file preview element.
+            paths: a dictionary in the format of '{ReportID: FilePath}' where 'FilePath' is the path
+                   to the file which should be included in the file preview for result 'ReportID'.
+
+        Returns:
+            The file preview element object ('_Tabs.FilePreviewDC').
         """
 
-        paths = {}
-        for reportid, fp in files.items():
-            if not fp.exists():
+        new_paths: dict[str, Path] = {}
+        for reportid, file_path in paths.items():
+            if not file_path.exists():
                 # If one of the reports does not have a file, exclude the file preview entirely.
-                _LOG.debug("file preview '%s' does not include report '%s' since the file '%s' "
-                           "doesn't exist.", title, reportid, fp)
+                _LOG.debug("File preview '%s' does not include report '%s' since the file '%s' "
+                           "doesn't exist", title, reportid, file_path)
                 continue
 
             # If the file is not in 'outdir' it should be copied to 'outdir'.
-            if self.outdir not in fp.parents:
-                if not _reasonable_file_size(fp, title):
+            if self._outdir not in file_path.parents:
+                if not _has_reasonable_size(file_path, title):
                     break
-                dst_path = self._copy_file(fp, reportid)
+                dst_path = self._copy_file(file_path, reportid)
             else:
-                dst_path = fp
+                dst_path = file_path
 
-            paths[reportid] = dst_path
+            new_paths[reportid] = dst_path
 
-        if not paths:
-            raise Error("unable to generate file preview")
+        if not new_paths:
+            raise Error(f"Unable to generate file preview '{title}'")
 
-        if len(paths) == 2:
+        if len(new_paths) == 2:
             try:
                 # Name the diff after one of the files.
-                diff_paths = list(paths.values())
+                diff_paths = list(new_paths.values())
                 diff_name = diff_paths[0].name
-                diff = self._generate_diff(diff_paths, diff_name)
+                diff = self._generate_html_diff(diff_paths, diff_name)
             except Error as err:
-                _LOG.info("Unable to generate diff for file preview '%s'.", title)
+                _LOG.info("Unable to generate diff for file preview '%s'", title)
                 _LOG.debug(err)
-                diff = ""
+                diff = None
         else:
-            diff = ""
+            diff = None
 
-        for reportid, path in paths.items():
-            paths[reportid] = path.relative_to(self._basedir)
+        for reportid, path in new_paths.items():
+            new_paths[reportid] = path.relative_to(self._basedir)
 
-        return _Tabs.FilePreviewDC(title, paths, diff)
+        return _Tabs.FilePreviewDC(title, new_paths, diff)
 
-    def __init__(self, outdir, basedir=None):
-        """Class constructor. Arguments are the same as in '_TabBuilderBase.TabBuilderBase()'."""
+    def __init__(self, outdir: Path, basedir: Path | None = None):
+        """
+        The class constructor.
 
-        self.outdir = outdir
+        Args:
+            outdir: The output directory path (where the file preview files should be placed).
+            basedir: The report base directory directory path, defaults to 'outdir'.
+        """
+
+        self._outdir = outdir
         self._basedir = basedir if basedir else outdir
-        self.fpreviews = []
