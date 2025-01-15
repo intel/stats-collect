60a72a8c4379b3d6cab129d2471d6bc80e3205d6
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-01-15 13:50:21 +0200

    SysInfo: refactor and modernise
    Refactor the module.
    1. Use type annotations.
    2. Use Google style docstrings.
    3. Use list of dictionaries for 'cmdinfos', instead of a dictionary, just
       because the keys of the dictionaries are unused, and therefore unnecessary,
       and a list can be used. List is a simpler data structure, so stick to it.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- statscollectlibs/collector/SysInfo.py
+++ statscollectlibs/collector/SysInfo.py
@@ -10,31 +10,53 @@
 Collect the system information statistics.
 """
 
+import typing
 import logging
+from pathlib import Path
 from pepclibs.helperlibs.Exceptions import Error
+from pepclibs.helperlibs.ProcessManager import ProcessManagerType
 
 _LOG = logging.getLogger()
 
-def _run_commands(cmdinfos, pman):
-    """Execute commands specified in the 'cmdinfos' dictionary."""
+class _CmdInfoDict(typing.TypedDict, total=False):
+    """
+    A dictionary that describes the commands that have to be run on the SUT to collect system
+    information.
+
+    Attributes:
+        cmd: the command to run.
+        outfile: path to the output file to save redirect the output of the command to.
+    """
+
+    cmd: str
+    outfile: Path
+
+def _run_commands(cmdinfos: list[_CmdInfoDict], pman: ProcessManagerType):
+    """
+    Execute commands specified in the 'cmdinfos' dictionary.
+
+    Args:
+        cmdinfos: describes the commands to execute.
+        pman: The process manager object that defines the host to execute the commands on.
+    """
 
     if pman.is_remote:
         # In case of a remote host, it is much more efficient to run all commands in one go, because
         # in this case only one SSH session needs to be established.
         cmd = ""
-        for cmdinfo in cmdinfos.values():
+        for cmdinfo in cmdinfos:
             cmd += cmdinfo["cmd"] + " &"
 
         cmd += " wait"
         try:
             pman.run_verify(cmd, shell=True)
         except Error as err:
-            _LOG.warning("some system statistics were not collected")
+            _LOG.warning("Some system statistics were not collected")
             _LOG.debug(str(err))
     else:
         procs = []
         errors = []
-        for cmdinfo in cmdinfos.values():
+        for cmdinfo in cmdinfos:
             try:
                 procs.append(pman.run_async(cmdinfo["cmd"], shell=True))
             except Error as err:
@@ -52,11 +74,19 @@ def _run_commands(cmdinfos, pman):
             _LOG.warning("not all the system statistics were collected, here are the failures\n%s",
                          "\nNext error:\n".join(errors))
 
-def _format_find_cmd(include, outfile, exclude=None):
+def _format_find_cmd(include: str, outfile: Path, exclude: str | None = None) -> str:
     """
-    Format and return a command which finds and dump the contents of files found using regular
-    expression 'include' into 'outfile'. If specified, exclude files matching the 'exclude' regular
-    expression.
+    Format and return a 'find' tool command which finds and dump the contents of files found using
+    regular expression 'include' into 'outfile'. If specified, exclude files matching the 'exclude'
+    regular expression.
+
+    Args:
+        include: The regular expression for file names the 'find' tool should find.
+        outfile: Path to the file the 'find' tool output should be redirected to.
+        exclude: The regular expression for file names to exclude from the found files list.
+
+    Returns:
+        The 'find' tool command.
     """
 
     cmd = fr"""find /sys/devices/system/cpu/ -type f -regex '.*/{include}/.*' """
@@ -65,163 +95,151 @@ def _format_find_cmd(include, outfile, exclude=None):
     cmd += fr"""-exec sh -c "echo '{{}}:'; cat '{{}}'; echo" \; > '{outfile}' 2>&1"""
     return cmd
 
-def _collect_sysinfo(outdir, when, pman):
+def _collect_sysinfo(outdir: Path, when: str, pman: ProcessManagerType):
     """
     Collecting the system information statistics which may change after a workload has been run on
-    the SUT. For example, 'dmesg' may have additional lines. The 'when' argument is supposed to be
-    either 'before' or 'after'.
+    the SUT. For example, 'dmesg' may have additional lines.
+
+    Args:
+        outdir: Path on the system defined by 'pman' to store the collected statistics in.
+        when: "before" if collecting before the workload starts, "after" if collecting after the
+              workload starts.
+        pman: The process manager object that defines the host to collect the statistics on.
     """
 
-    cmdinfos = {}
+    cmdinfos: list[_CmdInfoDict] = []
 
-    cmdinfos["cpuidle"] = cmdinfo = {}
     outfile = outdir / f"sys-cpuidle.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = _format_find_cmd("cpuidle", outfile)
+    cmd = _format_find_cmd("cpuidle", outfile)
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["cpufreq"] = cmdinfo = {}
     outfile = outdir / f"sys-cpufreq.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
     # Exclude 'scaling_cpu_freq' files -they are not very interesting.
-    cmdinfo["cmd"] = _format_find_cmd("cpufreq", outfile, exclude=".*/scaling_cur_freq")
+    cmd = _format_find_cmd("cpufreq", outfile, exclude=".*/scaling_cur_freq")
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["thermal_throttle"] = cmdinfo = {}
     outfile = outdir / f"sys-thermal_throttle.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = _format_find_cmd("thermal_throttle", outfile)
+    cmd = _format_find_cmd("thermal_throttle", outfile)
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["turbostat"] = cmdinfo = {}
     outfile = outdir / f"turbostat-d.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"turbostat -- sleep 1 > '{outfile}' 2>&1"
+    cmd = f"turbostat -- sleep 1 > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["turbostat_c0"] = cmdinfo = {}
     outfile = outdir / f"turbostat-d-c0.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"turbostat -c 0 -S -- sleep 0.01 > '{outfile}' 2>&1"
+    cmd = f"turbostat -c 0 -S -- sleep 0.01 > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["dmesg"] = cmdinfo = {}
     outfile = outdir / f"dmesg.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"dmesg --notime > '{outfile}' 2>&1"
+    cmd = f"dmesg --notime > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["x86_energy_perf_policy"] = cmdinfo = {}
     outfile = outdir / f"x86_energy_perf_policy.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"x86_energy_perf_policy > '{outfile}' 2>&1"
+    cmd = f"x86_energy_perf_policy > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["proc_interrupts"] = cmdinfo = {}
     outfile = outdir / f"interrupts.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"cat /proc/interrupts > '{outfile}' 2>&1"
+    cmd = f"cat /proc/interrupts > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["sysctl_all"] = cmdinfo = {}
     outfile = outdir / f"sysctl-all.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"sysctl --all > '{outfile}' 2>&1"
+    cmd = f"sysctl --all > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["pepc_cstates"] = cmdinfo = {}
     outfile = outdir / f"pepc_cstates.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"pepc cstates info > '{outfile}' 2>&1"
+    cmd = f"pepc cstates info > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["pepc_pstates"] = cmdinfo = {}
     outfile = outdir / f"pepc_pstates.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"pepc pstates info > '{outfile}' 2>&1"
+    cmd = f"pepc pstates info > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["pepc_pmqos"] = cmdinfo = {}
     outfile = outdir / f"pepc_pmqos.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"pepc pmqos info > '{outfile}' 2>&1"
+    cmd = f"pepc pmqos info > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["pepc_aspm"] = cmdinfo = {}
     outfile = outdir / f"pepc_aspm.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"pepc aspm info > '{outfile}' 2>&1"
+    cmd = f"pepc aspm info > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["pepc_topology"] = cmdinfo = {}
     outfile = outdir / f"pepc_topology.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"pepc topology info > '{outfile}' 2>&1"
+    cmd = f"pepc topology info > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["pepc_power"] = cmdinfo = {}
     outfile = outdir / f"pepc_power.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"pepc power info > '{outfile}' 2>&1"
+    cmd = f"pepc power info > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["lspci"] = cmdinfo = {}
     outfile = outdir / f"lspci.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"lspci > '{outfile}' 2>&1"
+    cmd = f"lspci > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["lspci_vvv"] = cmdinfo = {}
     outfile = outdir / f"lspci-vvv.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"lspci -vvv > '{outfile}' 2>&1"
+    cmd = f"lspci -vvv > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["lsusb"] = cmdinfo = {}
     outfile = outdir / f"lsusb.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"lsusb > '{outfile}' 2>&1"
+    cmd = f"lsusb > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["lsusb_v"] = cmdinfo = {}
     outfile = outdir / f"lsusb-v.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"lsusb -v > '{outfile}' 2>&1"
+    cmd = f"lsusb -v > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    return _run_commands(cmdinfos, pman)
+    _run_commands(cmdinfos, pman)
 
-def collect_before(outdir, pman):
+def collect_before(outdir: Path, pman: ProcessManagerType):
     """
-    Collect system information statistics before running the workload. The arguments are as follows.
-      * outdir - path on the system defined by 'pman' to store the collected statistics in.
-      * pman - the process manager object that defines the host to collect the statistics on.
+    Collect system information statistics before running the workload.
+
+    Args:
+        outdir: Path on the system defined by 'pman' to store the collected statistics in.
+        pman: The process manager object that defines the host to collect the statistics on.
     """
 
     pman.mkdir(outdir, parents=True, exist_ok=True)
 
     _collect_sysinfo(outdir, "before", pman)
 
-def collect_after(outdir, pman):
+def collect_after(outdir: Path, pman: ProcessManagerType):
     """
-    Collect system information statistics after running the workload. The arguments are as follows.
-      * outdir - path on the system defined by 'pman' to store the collected statistics in.
-      * pman - the process manager object that defines the host to collect the statistics on.
+    Collect system information statistics after running the workload.
+
+    Args:
+        outdir: Path on the system defined by 'pman' to store the collected statistics in.
+        pman: The process manager object that defines the host to collect the statistics on.
     """
 
-    cmdinfos = {}
+    cmdinfos: list[_CmdInfoDict] = []
     when = "after"
 
-    cmdinfos["proc_cmdline"] = cmdinfo = {}
     outfile = outdir / f"proc_cmdline.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"cat /proc/cmdline > '{outfile}' 2>&1"
+    cmd = f"cat /proc/cmdline > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["uname_a"] = cmdinfo = {}
+    outfile = outdir / f"proc_cmdline.{when}.raw.txt"
+    cmd = f"cat /proc/cmdline > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
+
     outfile = outdir / f"uname-a.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"uname -a > '{outfile}' 2>&1"
+    cmd = f"uname -a > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["dmidecode"] = cmdinfo = {}
     outfile = outdir / f"dmidecode.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"dmidecode > '{outfile}' 2>&1"
+    cmd = f"dmidecode > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["proc_cpuinfo"] = cmdinfo = {}
     outfile = outdir / f"proc_cpuinfo.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"cat /proc/cpuinfo > '{outfile}' 2>&1"
+    cmd = f"cat /proc/cpuinfo > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["lsmod"] = cmdinfo = {}
     outfile = outdir / f"lsmod.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"lsmod > '{outfile}' 2>&1"
+    cmd = f"lsmod > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
-    cmdinfos["lsblk"] = cmdinfo = {}
     outfile = outdir / f"lsblk.{when}.raw.txt"
-    cmdinfo["outfile"] = outfile
-    cmdinfo["cmd"] = f"lsblk > '{outfile}' 2>&1"
+    cmd = f"lsblk > '{outfile}' 2>&1"
+    cmdinfos.append(_CmdInfoDict(cmd=cmd, outfile=outfile))
 
     _run_commands(cmdinfos, pman)
 
