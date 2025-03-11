3276a0e3e643ed6fc0b973dc45259745ccfea11b
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-03-11 16:30:47 +0200

    DeployHelpersBase: annotate and modernize
    Annotate, use new coding style.
    
    Found a bug and added a TODO entry for it at the same time.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- statscollectlibs/deploylibs/DeployHelpersBase.py
+++ statscollectlibs/deploylibs/DeployHelpersBase.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 tw=100 et ai si
 #
-# Copyright (C) 2022-2023 Intel Corporation
+# Copyright (C) 2022-2025 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # Author: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
 
-"""This module provides a base class for deploying helpers."""
+"""
+Provide base class for deploying non-driver installables (helpers). Refer to the 'DeployBase' module
+docstring for terminology reference.
+"""
 
 import os
 from pathlib import Path
-from pepclibs.helperlibs import Logging, ProjectFiles
+from pepclibs.helperlibs import Logging, ProjectFiles, ToolChecker
 from pepclibs.helperlibs.Exceptions import Error
+from pepclibs.helperlibs.ProcessManager import ProcessManagerType
 from statscollectlibs.deploylibs import DeployInstallableBase
+from statscollectlibs.deploylibs.DeployBase import InstallableInfoType
 
 HELPERS_DEPLOY_SUBDIR = Path(".local")
 HELPERS_SRC_SUBDIR = Path("helpers")
@@ -20,21 +25,65 @@ HELPERS_SRC_SUBDIR = Path("helpers")
 _LOG = Logging.getLogger(f"{Logging.MAIN_LOGGER_NAME}.stats-collect.{__name__}")
 
 class DeployHelpersBase(DeployInstallableBase.DeployInstallableBase):
-    """This base class can be inherited from to provide the API for deploying helpers."""
+    """Base class for deploying non-driver installables (helpers)."""
 
-    def _prepare(self, helpersrc, helpers):
+    def __init__(self,
+                 prjname: str,
+                 toolname: str,
+                 what: str,
+                 spman: ProcessManagerType,
+                 bpman: ProcessManagerType,
+                 stmpdir: Path,
+                 btmpdir: Path,
+                 cpman: ProcessManagerType | None = None,
+                 ctmpdir: Path | None = None,
+                 btchk: ToolChecker.ToolChecker | None = None,
+                 debug: bool = False):
         """
-        Build and prepare helpers for deployment. The arguments are as follows:
-          * helpersrc - path to the helpers base directory on the controller.
-          * helpers - same as in the 'deploy()' method.
+        Initialize a class instance.
 
-        This method should be implemented by a child class.
+        Args:
+            prjname: Name of the project the helpers and 'toolname' belong to.
+            toolname: Name of the tool the helpers belong to.
+            spman: A process manager object associated with the SUT (System Under Test).
+            bpman: A process manager object associated with the build host (the host where the
+                   helper should be built).
+            stmpdir: A temporary directory on the SUT.
+            btmpdir: Path to a temporary directory on the build host.
+            cpman: A process manager object associated with the controller (local host). Defaults to
+                   'spman'.
+            ctmpdir: Path to a temporary directory on the controller. Defaults to 'stmpdir'.
+            btchk: An instance of 'ToolChecker' that can be used for checking the availability of
+                   various tools on the build host. Will be created if not provided.
+            debug: A boolean variable for enabling additional debugging messages.
+        """
+
+        self._what = what
+        super().__init__(prjname, toolname, spman, bpman, stmpdir, btmpdir, cpman=cpman,
+                         ctmpdir=ctmpdir, btchk=btchk, debug=debug)
+
+    def _prepare(self, insts_info: dict[str, InstallableInfoType], installables_basedir: Path):
+        """
+        Build and prepare installables for deployment.
+
+        Args:
+            insts_info: The installables information dictionary.
+            installables_basedir: Path to the base directory that contains the installables on the
+                                  controller.
+
+        Raises:
+            NotImplementedError: The method is not implemented by the subclass.
         """
 
         raise NotImplementedError()
 
-    def _get_helpers_deploy_path(self):
-        """Returns path the directory the helpers should be deployed to."""
+    def _get_deploy_path(self) -> Path:
+        """
+        Return the path to the directory where the helper deployables should be deployed.
+
+        Returns:
+            Path: The path to the directory where the helper programs should be deployed.
+        """
 
         envvar = ProjectFiles.get_project_helpers_envvar("stats-collect")
         stdout, _ = self._spman.run_verify(f"echo ${envvar}")
@@ -45,35 +94,32 @@ class DeployHelpersBase(DeployInstallableBase.DeployInstallableBase):
             helpers_path = self._spman.get_homedir() / HELPERS_DEPLOY_SUBDIR / "bin"
         return Path(helpers_path)
 
-    def deploy(self, helpers):
+    def deploy(self, insts_info: dict[str, InstallableInfoType]):
         """
-        Deploy helpers to the SUT. The arguments are as follows.
-          * helpers - the helpers to deploy. Should be a collection of helper names (e.g., a list).
-                      These are not deployable names, these are helper directory names in the
-                      'HELPERS_DEPLOY_SUBDIR' sub-directory.
+        Deploy installables to the System Under Test (SUT).
 
-        This is the default implementation which deploys the helpers by running 'make' and 'make
-        install'.
+        Args:
+            insts_info: The installables information dictionary.
         """
 
-        if not helpers:
-            return
-
-        # We assume all helpers are in the same base directory.
-        helper_path = HELPERS_SRC_SUBDIR/f"{helpers[0]}"
+        # Find the installables base directory. We assume all installables are located in the same
+        # base directory.
+        some_installable = next(iter(insts_info))
+        some_installable_path = HELPERS_SRC_SUBDIR/f"{some_installable}"
         what = f"sources of {self._what}"
-        helpersrc = ProjectFiles.find_project_data(self._prjname, helper_path, what=what)
-        helpersrc = helpersrc.parent
+        some_installable_path = ProjectFiles.find_project_data(self._prjname, some_installable_path,
+                                                               what=what)
+        installables_basedir = some_installable_path.parent
 
         # Make sure all helpers are available.
-        for helper in helpers:
-            helperdir = helpersrc / helper
-            if not helperdir.is_dir():
-                raise Error(f"path '{helperdir}' does not exist or it is not a directory")
+        for installable in insts_info:
+            installabledir = installables_basedir / installable
+            if not installabledir.is_dir():
+                raise Error(f"Path '{installabledir}' does not exist or it is not a directory")
 
-        self._prepare(helpersrc, helpers)
+        self._prepare(insts_info, installables_basedir)
 
-        deploy_path = self._get_helpers_deploy_path()
+        deploy_path = self._get_deploy_path()
 
         # Make sure the the destination deployment directory exists.
         self._spman.mkdir(deploy_path, parents=True, exist_ok=True)
@@ -81,35 +127,24 @@ class DeployHelpersBase(DeployInstallableBase.DeployInstallableBase):
         # Deploy all helpers.
         _LOG.info("Deploying %s to '%s'%s", self._what, deploy_path, self._spman.hostmsg)
 
-        helpersdst = self._stmpdir / "helpers_deployed"
-        _LOG.debug("deploying helpers to '%s'%s", helpersdst, self._spman.hostmsg)
+        for installable in insts_info:
+            installable_dstdir = self._stmpdir / f"{installable}-deployed"
+            _LOG.debug("deploying helper installable helper '%s' to '%s'%s",
+                       installable, installable_dstdir, self._spman.hostmsg)
 
-        for helper in helpers:
-            bhelperpath = f"{self._btmpdir}/{helper}"
-            shelperpath = f"{self._stmpdir}/{helper}"
+            binstpath = f"{self._btmpdir}/{installable}"
+            sinstpath = f"{self._stmpdir}/{installable}"
 
             if not self._bpman.is_remote and self._spman.is_remote:
-                # We built the helpers locally, but have to install them on a remote SUT. Copy them
-                # to the SUT first.
-                self._spman.rsync(str(bhelperpath) + "/", shelperpath,
+                # The installables were built locally (in 'self._prepare()), but they should be
+                # installed on the SUT. Copy them to the SUT first.
+                self._spman.rsync(str(binstpath) + "/", sinstpath,
                                   remotesrc=self._bpman.is_remote, remotedst=self._spman.is_remote)
 
-            cmd = f"make -C '{shelperpath}' install PREFIX='{helpersdst}'"
+            cmd = f"make -C '{sinstpath}' install PREFIX='{installable_dstdir}'"
             stdout, stderr = self._spman.run_verify(cmd)
             self._log_cmd_output(stdout, stderr)
 
-            self._spman.rsync(str(helpersdst) + "/bin/", deploy_path,
+            self._spman.rsync(str(installable_dstdir) + "/bin/", deploy_path,
                               remotesrc=self._spman.is_remote,
                               remotedst=self._spman.is_remote)
-
-    def __init__(self, prjname, toolname, what, spman, bpman, stmpdir, btmpdir, cpman=None,
-                 ctmpdir=None, btchk=None, debug=False):
-        """
-        Class constructor. Arguments are the same as in
-        'DeployInstallableBase.DeployInstallableBase()' except for the following:
-         * what - a human-readable string describing the helpers that are being deployed.
-        """
-
-        self._what = what
-        super().__init__(prjname, toolname, spman, bpman, stmpdir, btmpdir, cpman=cpman,
-                         ctmpdir=ctmpdir, btchk=btchk, debug=False)
