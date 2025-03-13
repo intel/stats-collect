68b9eb9c32d711ddf7e3408ffb302fe0bd6e832b
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-03-13 18:08:46 +0200

    tests: address pylint warning
    A line was too long.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- tests/test_module_InterruptsParser.py
+++ tests/test_module_InterruptsParser.py
@@ -39,7 +39,8 @@ def test_cut_files():
 
         assert len(first_snapshot["cpu2irqs"]) == len(last_snapshot["cpu2irqs"]), \
                f"{pfx}: Expected the same number of CPUs, but got " \
-               f"{Trivial.rangify(first_snapshot['cpus'])} and {Trivial.rangify(last_snapshot['cpus'])}"
+               f"{Trivial.rangify(first_snapshot['cpus'])} and " \
+               f"{Trivial.rangify(last_snapshot['cpus'])}"
 
         assert len(first_snapshot["irq_info"]) == len(last_snapshot["irq_info"]), \
                f"{pfx}: Expected the same number of IRQs, but got " \
