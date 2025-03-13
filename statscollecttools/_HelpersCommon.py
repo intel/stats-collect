5eca6fa6e7e76b05eaa4efd36f9ed9123239b107
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-03-13 10:00:59 +0200

    stc-agent: separate out 'print_module_paths()'
    Move 'print_module_paths()' to a separate module, because it will be shared
    with other helpers soon.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

---
+++ statscollecttools/_HelpersCommon.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+#
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+#
+# Copyright (C) 2022-2025 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Author: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
+
+import sys
+
+def print_module_paths():
+    """
+    Print paths to all modules other than standard.
+    """
+
+    subpaths = ("pepclibs/", "statscollectlibs/", "statscollecttools/")
+
+    for mobj in sys.modules.values():
+        path = getattr(mobj, "__file__", None)
+        if not path:
+            continue
+
+        if not path.endswith(".py"):
+            continue
+
+        for subpath in subpaths:
+            if subpath in path:
+                print(path)
