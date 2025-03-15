27cd593094891a0c3d408d52f7d44e5faece2a98
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-03-15 17:20:02 +0200

    _StatsCollectReport: refactor to use new coding style
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- statscollecttools/_StatsCollectReport.py
+++ statscollecttools/_StatsCollectReport.py
@@ -13,7 +13,9 @@ The 'stats-collect report' command implementation.
 
 from __future__ import annotations # Remove when switching to Python 3.10+.
 
+import argparse
 from pathlib import Path
+from typing import NamedTuple
 from pepclibs.helperlibs import Trivial
 from pepclibs.helperlibs.Exceptions import Error
 from statscollectlibs.helperlibs import ReportID
@@ -21,39 +23,34 @@ from statscollectlibs.rawresultlibs import RORawResult
 from statscollectlibs.htmlreport import _StatsCollectHTMLReport
 from statscollecttools import ToolInfo, _Common
 
-def open_raw_results(respaths: list[Path],
-                     reportids: str | None = None,
-                     cpus: str | None = None) -> list[RORawResult.RORawResult]:
+class _ReportCommandArgsType(NamedTuple):
+    """The "stats-collect report" command-line arguments named tuple type."""
+
+    outdir: Path
+    reportids: list[str]
+    copy_raw: bool
+    respaths: list[Path]
+    cpus: list[int] | None
+
+def _open_raw_results(args: _ReportCommandArgsType) -> list[RORawResult.RORawResult]:
     """
     Open the input raw test results and return a list of 'RORawResult' objects.
 
     Args:
-        respaths: List of paths to raw results.
-        reportids: Comma-separated list of report IDs to override report IDs in raw results.
-        cpus: Comma-separated list of CPU numbers to include in the report.
+        args: The command-line arguments.
 
     Returns:
         list[RORawResult.RORawResult]: List of 'RORawResult' objects.
     """
 
-    if reportids:
-        rids = Trivial.split_csv_line(reportids)
-    else:
-        rids = []
+    rids = list(args.reportids)
 
-    if len(rids) > len(respaths):
-        raise Error(f"There are {len(rids)} report IDs to assign to {len(respaths)} input "
-                    f"test results. Please, provide {len(respaths)} or fewer report IDs.")
-
-    if cpus:
-        cpus = Trivial.split_csv_line_int(cpus, what="--cpus argument")
-
-    # Append the required amount of 'None's to make the 'rids' list be of the same length as
+    # Append the required amount of empty strings to make the 'rids' list be of the same length as #
     # the 'respaths' list.
-    rids += [None] * (len(respaths) - len(rids))
+    rids += [""] * (len(args.respaths) - len(rids))
 
     rsts = []
-    for respath, reportid in zip(respaths, rids):
+    for respath, reportid in zip(args.respaths, rids):
         if reportid:
             ReportID.validate_reportid(reportid)
 
@@ -61,24 +58,69 @@ def open_raw_results(respaths: list[Path],
         if ToolInfo.TOOLNAME != res.info["toolname"]:
             raise Error(f"Cannot generate '{ToolInfo.TOOLNAME}' report, results are collected with "
                         f"'{res.info['toolname']}':\n{respath}")
-        if cpus:
-            res.info["cpus"] = cpus
+        if args.cpus:
+            res.info["cpus"] = args.cpus
+
         rsts.append(res)
 
     return rsts
 
-def report_command(args):
+def _format_args(arguments: argparse.Namespace) -> _ReportCommandArgsType:
+    """
+    Validate and format the 'stats-collect report' tool input command-line arguments, then build and
+    return the arguments named tuple object.
+
+    Args:
+        arguments: The input arguments parsed from the command line.
+
+    Returns:
+        _ReportCommandArgsType: A named tuple containing the formatted arguments.
+    """
+
+    if len(arguments.respaths) == 0:
+        # This should have been ensured by the command line parser.
+        raise Error("BUG: no raw results paths provided")
+
+    if not arguments.outdir:
+        outdir = arguments.respaths[0] / "html-report"
+    else:
+        outdir = arguments.outdir
+
+    reportids: list[str] = []
+    if arguments.reportids:
+        reportids = Trivial.split_csv_line(arguments.reportids)
+
+    respaths: list[Path] = []
+    if arguments.respaths:
+        respaths = arguments.respaths
+
+    if len(reportids) > len(respaths):
+        raise Error(f"There are {len(reportids)} report IDs to assign to {len(respaths)} input "
+                    f"test results. Please, provide {len(respaths)} or fewer report IDs.")
+
+    cpus: list[int] | None = []
+    if arguments.cpus:
+        cpus = Trivial.split_csv_line_int(arguments.cpus, what="--cpus argument")
+
+    return _ReportCommandArgsType(
+        outdir = outdir,
+        reportids = reportids,
+        copy_raw = arguments.copy_raw,
+        respaths = respaths,
+        cpus = cpus,
+    )
+
+def report_command(arguments):
     """
     Implements the 'report' command.
 
     Args:
-        args: The command-line arguments.
+        arguments: The command-line arguments.
     """
 
-    rsts = open_raw_results(args.respaths, reportids=args.reportids, cpus=args.cpus)
+    args = _format_args(arguments)
 
-    if not args.outdir:
-        args.outdir = args.respaths[0] / "html-report"
+    rsts = _open_raw_results(args)
 
     logpath = _Common.configure_log_file(args.outdir, ToolInfo.TOOLNAME)
     logpath = Path(logpath).relative_to(args.outdir)
