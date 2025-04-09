9e50109143b43b52eb4fed442046ac1a94756d13
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-04-09 14:58:10 +0300

    _CapturedOutputTabBuilder: improve annotation and modernize
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- statscollectlibs/htmlreport/tabs/_CapturedOutputTabBuilder.py
+++ statscollectlibs/htmlreport/tabs/_CapturedOutputTabBuilder.py
@@ -31,16 +31,33 @@ class CapturedOutputTabBuilder():
     """
     The tab builder class for the "Captured Output" tab, which just shows and compares the stdout
     and stderr of the workload(s).
-
-    Public methods overview:
-     * 'get_tab()' - Generate a 'BuiltTab.BuiltDTab' instance containing file previews of the
-                     captured 'stdout' and 'stderr' logs from 'stats-collect start'.
     """
 
     name = "Captured Output"
 
-    def _write_lines(self, lines, dstpath):
-        """Helper function for 'generate_captured_output_tab()'."""
+    def __init__(self, lrsts: list[LoadedResult], outdir: Path, basedir: Path | None = None):
+        """
+        Initialize a class instance.
+
+        Args:
+            lrsts: List of loaded test result objects to include in the tab.
+            outdir: Output directory where the sub-directory for tab data files will be created.
+            basedir: Base directory of the report. All HTML links in the tab will be made relative
+                     to this directory. Defaults to 'outdir' if not provided.
+        """
+
+        self._lrsts = lrsts
+        self._basedir = basedir if basedir else outdir
+        self._outdir = outdir / _DTabBuilder.get_fsname(self.name)
+
+    def _write_lines(self, lines: list[str], dstpath: Path) -> None:
+        """
+        Write the provided lines to the specified destination path.
+
+        Args:
+            lines: A list of strings representing the lines to be written to the file.
+            dstpath: The destination path where the lines will be written.
+        """
 
         try:
             dstpath.parent.mkdir(parents=True, exist_ok=True)
@@ -48,41 +65,54 @@ class CapturedOutputTabBuilder():
                 f.writelines(lines)
         except OSError as err:
             errmsg = Error(str(err)).indent(2)
-            errmsg = f"unable to write trimmed captured output file at '{dstpath}':\n{errmsg}"
-            raise Error(errmsg) from None
+            raise Error(f"unable to write trimmed captured output file at '{dstpath}':\n"
+                        f"{errmsg}") from None
 
-    def _trim_lines(self, lines, top, bottom):
+    def _trim_file_lines(self, lines: list[str], top: int, bottom: int) -> list[str]:
         """
-        Copy the file at 'srcpath' to 'dstpath' and removes all but the top 'top' lines and bottom
-        'bottom' lines. Return a boolean representing if the file was trimmed or not.
+        Trim the provided list of lines to include only the first 'top' lines and the last 'bottom'
+        lines.
+
+        Args:
+            lines: The lines of a file to be trimmed.
+            top: Number of lines to preserve from the start of the file.
+            bottom: Number of lines to preserve from the end of the file.
+
+        Returns:
+            A list of strings representing the trimmed lines of the file.
         """
 
         trim_notice_lines = [
             "==========================\n",
             "FILE CONTENTS REMOVED HERE\n",
             "==========================\n"
-            ]
+        ]
 
+        # If the total number of lines is less than or equal to the sum of 'top' and 'bottom',
+        # no trimming is necessary.
         if len(lines) <= top + bottom:
             trimmed_lines = lines
         else:
+            # Preserve the top 'top' lines, add a notice, and preserve the bottom 'bottom' lines.
             trimmed_lines = lines[:top] + trim_notice_lines + lines[-bottom:]
 
         return trimmed_lines
 
-    def build_tab(self):
+    def build_tab(self) -> BuiltTab.BuiltCTab:
         """
-        Return a 'BuiltTab.BuiltDTab' instance containing file previews of the captured 'stdout' and
-        'stderr' logs from 'stats-collect start'.
+        Build the tab and return a built C-tab object ('BuiltTab.BuiltCTab'). The resulting tab will
+        include file previews of the captured 'stdout' and 'stderr' logs from 'stats-collect start'.
         """
 
-        _LOG.info("Generating '%s' tab.", self.name)
+        _LOG.info("Generating '%s' tab", self.name)
 
         fpbuilder = FilePreviewBuilder.FilePreviewBuilder(self._outdir, basedir=self._basedir)
-        fpreviews = []
-        trimmed_lrsts = set()
+        fpreviews: list[BuiltTab.BuiltDTabFilePreview] = []
+        trimmed_reportids: set[str] = set()
+
         for stream_name in ("stdout", "stderr"):
-            files = {}
+            files: dict[str, Path] = {}
+
             for lres in self._lrsts:
                 if stream_name not in lres.res.info:
                     continue
@@ -97,34 +127,35 @@ class CapturedOutputTabBuilder():
                     continue
 
                 try:
-                    with open(srcpath, "r", encoding="utf-8") as f:
-                        lines = f.readlines()
+                    with open(srcpath, "r", encoding="utf-8") as fobj:
+                        lines = fobj.readlines()
                 except OSError as err:
-                    raise Error(f"unable to open captured output file at '{srcpath}':\n"
+                    raise Error(f"Failed to open captured output file at '{srcpath}':\n"
                                 f"{Error(str(err)).indent(2)}") from None
 
-                trimmed_lines = self._trim_lines(lines, _MAX_FILE_START, _MAX_FILE_END)
+                trimmed_lines = self._trim_file_lines(lines, _MAX_FILE_START, _MAX_FILE_END)
 
                 if len(trimmed_lines) < len(lines):
-                    trimmed_lrsts.add(lres.reportid)
+                    trimmed_reportids.add(lres.reportid)
                     dstpath = resdir / f"trimmed-{srcpath.name}"
                 else:
                     dstpath = resdir / srcpath.name
 
                 self._write_lines(trimmed_lines, dstpath)
-
                 files[lres.reportid] = dstpath
 
             if files:
                 fpreviews.append(fpbuilder.build_fpreview(stream_name, files))
 
         if not fpreviews:
-            return None
+            raise Error("No captured output files found in the raw results")
 
-        if trimmed_lrsts:
+        if trimmed_reportids:
+            # Add a notice about trimmed contents to the HTML report.
+            #
             # Convert the set of report IDs into a list which maintains the order of reports used
             # elsewhere.
-            trimmed = [lres.reportid for lres in self._lrsts if lres.reportid in trimmed_lrsts]
+            trimmed = [lres.reportid for lres in self._lrsts if lres.reportid in trimmed_reportids]
             msg = f"Note - the outputs of the following results have been trimmed to save time " \
                   f"during report generation: {', '.join(trimmed)}"
             alerts = [msg]
@@ -133,17 +164,3 @@ class CapturedOutputTabBuilder():
 
         dtab = BuiltTab.BuiltDTab(self.name, fpreviews=fpreviews, alerts=alerts)
         return BuiltTab.BuiltCTab(self.name, tabs=[dtab])
-
-    def __init__(self, lrsts: list[LoadedResult], outdir: Path, basedir: Path | None = None):
-        """
-        The class constructor. The arguments are as follows.
-         * lrsts - a list of loaded test result objects for which data should be included in the
-                   tab.
-         * outdir - the output directory in which to create the sub-directory for the container tab.
-         * basedir - base directory of the report. All paths should be made relative to this.
-                     Defaults to 'outdir'.
-        """
-
-        self._lrsts = lrsts
-        self._basedir = basedir if basedir else outdir
-        self._outdir = outdir / _DTabBuilder.get_fsname(self.name)
